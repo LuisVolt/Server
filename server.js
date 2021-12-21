3731ff6d4a6fe465aa49393df0198b00bd573407
LuisVolt <95005964+LuisVolt@users.noreply.github.com>
2021-12-20 20:21:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const http = require('http');
 
-const port = 3000;
+const port1 = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.edn('Hello, World');
-}).listen(port);
+});
 
-server.listen(port, () => {
-  console.log('Server running on port: ${port}');
+server.listen(port1, () => {
+  console.log('Server running on port: ${port1}');
 });
