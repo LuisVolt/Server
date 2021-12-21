c1e0192c55356d0da50dd0b411c2f20549a9749c
LuisVolt <95005964+LuisVolt@users.noreply.github.com>
2021-12-20 20:36:54 -0600

    adding date
    importing a module to show date

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const http = require('http');
+var dt = require('./1stmodule');
 
-const port1 = 3000;
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  res.write("The date and time are currently: " + dt.myDateTime());
   res.end('Hello, World');
 });
 
-server.listen(port1, () => {
-  console.log('Server running on port: ${port1}');
+server.listen(port, () => {
+  console.log('Server running on port: ${port}');
 });
