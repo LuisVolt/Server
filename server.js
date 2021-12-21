43b9f0ae1d5f0d3233370057695f2351e112703c
LuisVolt <95005964+LuisVolt@users.noreply.github.com>
2021-12-20 20:02:24 -0600

    Update server.js
    Change hhtp to  http on line five and  createerver to createServer

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const port = 3000;
 
-const server = hhtp.createerver((req, res) => {
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.edn('Hello, World');
