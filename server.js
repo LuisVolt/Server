7d339f730baf0b8f2f55b5131ce776c49fa8689c
LuisVolt <95005964+LuisVolt@users.noreply.github.com>
2021-12-20 21:04:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 var dt = require('./firstmodule');
 
-const port = 3000;
+const port = 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
