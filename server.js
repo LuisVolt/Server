0d81ecd4e8b4efc6feae01b6c668dd285a0ea750
LuisVolt <95005964+LuisVolt@users.noreply.github.com>
2021-12-20 19:42:46 -0600

    Create server.js
    Server on node.js for AWS trial

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const port = 3000;
+
+const server = hhtp.createerver((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.edn('Hello, World);
+});
+
+server.listen(port, () => {
+  console.log('Server running on port: ${port]';
+});
